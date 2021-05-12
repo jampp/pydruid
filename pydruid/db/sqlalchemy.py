443ad35a349db4435881a5e7f3db1557393e5f52
Mauricio Guignard <mauricio.guignard@jampp.com>
2021-05-12 10:51:40 -0300

    Avoid setting header parameter if not in SQLAlchemy URI

--- pydruid/db/sqlalchemy.py
+++ pydruid/db/sqlalchemy.py
@@ -115,8 +115,12 @@ class DruidDialect(default.DefaultDialect):
             "path": url.database,
             "scheme": self.scheme,
             "context": self.context,
-            "header": url.query.get("header") != "false",
         }
+
+        header = url.query.get("header")
+        if header is not None:
+            kwargs["header"] = header != "false"
+
         return ([], kwargs)
 
     def get_schema_names(self, connection, **kwargs):
