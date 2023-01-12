e5756945ede5e702b80490e6f97a8088d5815e3c
Mauricio Guignard <mauricio.guignard@jampp.com>
2023-01-12 09:26:18 -0300

    Fix package version not compliant with PEP440

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with io.open("README.md", encoding="utf-8") as f:
 
 setup(
     name="pydruid",
-    version="0.9.0j",
+    version="0.9.0+j",
     author="Druid Developers",
     author_email="druid-development@googlegroups.com",
     packages=find_packages(),
